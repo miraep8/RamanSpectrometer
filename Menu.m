94c597e0ec52df6457a4758c95b472ced1dd3fea
PARKER Mirae <miraep8@gmail.com>
2016-08-08 13:26:17 +0200

    more variable name-changing

--- Menu.m
+++ Menu.m
@@ -11,23 +11,23 @@ classdef Menu < handle
     
     properties
         
-        Body                    %A GUI figure which stores the GUI objects
-        Trans_Button            %The button which opens the Tranmittion interface
-        Trans_Pic               %The axis holding the snazzy transmition image
-        Abs_Button              %The button which opens the Absorbtion interface
-        Abs_Pic                 %Not what it sounds like, just another image storing axis
-        Reflect_Button          %The button which opens the Reflection interface
-        Reflect_Pic             %Axis storing the Reflect image
-        Raman_Button            %The button which opens the Raman interface
-        Raman_Pic               %Axis which stores the Raman image
-        Background              %Axis which will store the professional-looking backdrop
+        body                    %A GUI figure which stores the GUI objects
+        trans_Button            %The button which opens the Tranmittion interface
+        trans_Pic               %The axis holding the snazzy transmition image
+        abs_Button              %The button which opens the Absorbtion interface
+        abs_Pic                 %Not what it sounds like, just another image storing axis
+        reflect_Button          %The button which opens the Reflection interface
+        reflect_Pic             %Axis storing the Reflect image
+        raman_Button            %The button which opens the Raman interface
+        raman_Pic               %Axis which stores the Raman image
+        background              %Axis which will store the professional-looking backdrop
         
         
-        Picture                 %Stores the background picture for the Menu App
-        T_Icon                  %Stores the transmittion picture
-        A_Icon                  %Stores the Absorbance Picture
-        Ra_Icon                 %Stores the Raman Picture
-        Re_Icon                 %Stores the Reflected Icon
+        picture                 %Stores the background picture for the Menu App
+        t_Icon                  %Stores the transmittion picture
+        a_Icon                  %Stores the Absorbance Picture
+        ra_Icon                 %Stores the Raman Picture
+        re_Icon                 %Stores the Reflected Icon
         
        
     end
@@ -39,16 +39,16 @@ classdef Menu < handle
         function menu = Menu
             
             
-            menu.Body = figure('Position', [350, 160, 900, 550]);
-            menu.Background = axes('Parent', menu.Body, 'Position', [0,0,1,1]);
-            menu.Picture = imread('Images/Menu_Backdrop.png');
-            image(menu.Background, menu.Picture)
+            menu.body = figure('Position', [350, 160, 900, 550]);
+            menu.background = axes('Parent', menu.body, 'Position', [0,0,1,1]);
+            menu.picture = imread('Images/Menu_Backdrop.png');
+            image(menu.background, menu.picture)
             axis off
             
-            menu.Trans_Button = uicontrol(menu.Body, 'ForegroundColor', [1,1,1],  'BackgroundColor', [.05, .05, .4], 'Style', 'pushbutton', 'Position', [715, 270, 170, 40], 'String', 'Transmittance','FontSize', 20, 'Callback', @menu.transButton_Callback);
-            menu.Reflect_Button = uicontrol(menu.Body, 'ForegroundColor', [1,1,1],  'BackgroundColor', [.1, .1, .4], 'Style', 'pushbutton', 'Position', [490, 270, 170, 40], 'String', 'Reflectance','FontSize', 20, 'Callback', @menu.reflectButton_Callback);
-            menu.Abs_Button = uicontrol(menu.Body, 'ForegroundColor', [1,1,1], 'BackgroundColor', [.15, .15, .4], 'Style', 'pushbutton', 'Position', [260, 270, 170, 40], 'String', 'Absorbance','FontSize', 20, 'Callback', @menu.absButton_Callback);
-            menu.Raman_Button = uicontrol(menu.Body, 'ForegroundColor', [1,1,1], 'BackgroundColor', [.2, .2, .4], 'Style', 'pushbutton', 'Position', [25, 270, 170, 40], 'String', 'Raman','FontSize', 20, 'Callback', @menu.ramanButton_Callback);
+            menu.trans_Button = uicontrol(menu.body, 'ForegroundColor', [1,1,1],  'BackgroundColor', [.05, .05, .4], 'Style', 'pushbutton', 'Position', [715, 270, 170, 40], 'String', 'Transmittance','FontSize', 20, 'Callback', @menu.transButton_Callback);
+            menu.reflect_Button = uicontrol(menu.body, 'ForegroundColor', [1,1,1],  'BackgroundColor', [.1, .1, .4], 'Style', 'pushbutton', 'Position', [490, 270, 170, 40], 'String', 'Reflectance','FontSize', 20, 'Callback', @menu.reflectButton_Callback);
+            menu.abs_Button = uicontrol(menu.body, 'ForegroundColor', [1,1,1], 'BackgroundColor', [.15, .15, .4], 'Style', 'pushbutton', 'Position', [260, 270, 170, 40], 'String', 'Absorbance','FontSize', 20, 'Callback', @menu.absButton_Callback);
+            menu.raman_Button = uicontrol(menu.body, 'ForegroundColor', [1,1,1], 'BackgroundColor', [.2, .2, .4], 'Style', 'pushbutton', 'Position', [25, 270, 170, 40], 'String', 'Raman','FontSize', 20, 'Callback', @menu.ramanButton_Callback);
             
             end
             
