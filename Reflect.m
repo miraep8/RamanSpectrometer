1aee738837e337d558435884952eff12191d2d35
PARKER Mirae <miraep8@gmail.com>
2016-08-11 15:00:28 +0200

    working on Reflect class

--- Reflect.m
+++ Reflect.m
@@ -1,4 +1,5 @@
 classdef Reflect < spectra_Class
+    
         %Trans  This class is an extension of the Spectra Class.  Its function
     %is to create the specialized type of plot that is specific for a
     %Transmittion plots.
@@ -16,6 +17,13 @@ classdef Reflect < spectra_Class
     end
     
     methods
+        
+        function reflect = Reflect
+            
+            reflect@spectra_Class;
+            
+            while
+            
         
     end
 end
